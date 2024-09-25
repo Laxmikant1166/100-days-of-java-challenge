897fe8d06276719804b35aa7843a11e546d2dc29
Laxmikant Ghodake <151601776+Laxmikant1166@users.noreply.github.com>
2024-09-25 22:23:38 +0530

    Add files via upload

---
+++ Day3.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Day3 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+       
+        System.out.println("Enter the age:");
+        int age = sc.nextInt();
+
+        System.out.println("Enter the percentage:");
+        Double percentage = sc.nextDouble();
+
+        System.out.println("Enter the name:");
+        String name = sc.next();
+
+        System.out.println("name="+name);
+        System.out.println("Age="+age);
+        System.out.println("Percentage="+percentage);
+    }
+    
+}
